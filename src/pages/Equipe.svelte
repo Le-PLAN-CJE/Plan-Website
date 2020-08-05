2450df5c7b6ca1b5b724f8551f96eeb437212ec7
STR1Z <weihengjiang64@hotmail.com>
2020-08-05 16:10:55 -0400

    Co-authored-by: bogdansava363@gmail.com <bogdansava363@gmail.com>

--- src/pages/Equipe.svelte
+++ src/pages/Equipe.svelte
@@ -1,57 +1,50 @@
+<script>
+  export let res;
+</script>
+
 <h2>Équipe</h2>
 <div
   class="uk-grid-column-small uk-grid-row-large uk-child-width-1-2
-  uk-child-width-1-3@s uk-child-width-1-4@m uk-child-width-1-5@l uk-text-center"
+  uk-child-width-1-3@s uk-child-width-1-4@m uk-child-width-1-5@l uk-text-center
+  uk-flex-center"
   uk-grid>
 
-  <div
-    class="uk-position-relative uk-visible-toggle"
-    tabindex="-1"
-    uk-slideshow="animation: push; autoplay: true; ratio: 2:3">
-    <ul class="uk-slideshow-items">
-      <li>
-        <img
-          src="https://scontent.fymy1-1.fna.fbcdn.net/v/t1.0-9/53510674_107899557036405_7071252233651748864_o.jpg?_nc_cat=107&_nc_sid=09cbfe&_nc_ohc=wLvMSqvvvxkAX8Td44G&_nc_ht=scontent.fymy1-1.fna&oh=0a8288f2f6d96517bd8cc3e01453fe86&oe=5F478612"
-          alt=""
-          uk-cover />
-        <div
-          class="uk-overlay uk-overlay-primary uk-position-bottom uk-text-center
-          uk-transition-slide-bottom">
-          <h3 class="uk-margin-remove">President</h3>
-          <p class="uk-margin-remove">Le Bogdan</p>
+  {#each res.data as slideshow}
+    <div>
+      <h4>{slideshow.title}</h4>
+      <div
+        class="uk-position-relative uk-visible-toggle"
+        tabindex="-1"
+        uk-slideshow="animation: push; autoplay: true; ratio: 2:3">
+        <ul class="uk-slideshow-items">
+          {#each slideshow.members as member}
+            <li>
+              <img style="height: 100%" src={member.imageURL} alt="" uk-cover />
+              <div
+                class="uk-overlay uk-overlay-primary uk-position-bottom
+                uk-text-center uk-transition-slide-bottom">
+                <h4 class="uk-margin-remove">{member.name}</h4>
+                <p class="uk-margin-remove">
+                  <small>{member.roles.join(', ')}</small>
+                </p>
+              </div>
+            </li>
+          {/each}
+        </ul>
+        <div class="uk-light">
+          <a
+            class="uk-position-center-left uk-position-small uk-hidden-hover"
+            href="#"
+            uk-slidenav-previous
+            uk-slideshow-item="previous" />
+          <a
+            class="uk-position-center-right uk-position-small uk-hidden-hover"
+            href="#"
+            uk-slidenav-next
+            uk-slideshow-item="next" />
         </div>
-      </li>
-
-      <li>
-        <img
-          src="https://scontent.fymy1-1.fna.fbcdn.net/v/t1.0-9/53510674_107899557036405_7071252233651748864_o.jpg?_nc_cat=107&_nc_sid=09cbfe&_nc_ohc=wLvMSqvvvxkAX8Td44G&_nc_ht=scontent.fymy1-1.fna&oh=0a8288f2f6d96517bd8cc3e01453fe86&oe=5F478612"
-          alt=""
-          uk-cover />
-        <div
-          class="uk-overlay uk-overlay-primary uk-position-bottom uk-text-center
-          uk-transition-slide-bottom">
-          <h3 class="uk-margin-remove">Bottom</h3>
-          <p class="uk-margin-remove">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          </p>
-        </div>
-      </li>
-
-    </ul>
-
-    <div class="uk-light">
-      <a
-        class="uk-position-center-left uk-position-small uk-hidden-hover"
-        href="#"
-        uk-slidenav-previous
-        uk-slideshow-item="previous" />
-      <a
-        class="uk-position-center-right uk-position-small uk-hidden-hover"
-        href="#"
-        uk-slidenav-next
-        uk-slideshow-item="next" />
+      </div>
     </div>
-
-  </div>
+  {/each}
 
 </div>
