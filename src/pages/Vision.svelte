83b92fcb9d4e439384eeb48dfd607e598259b06f
okieez <bogdansava363@gmail.com>
2020-10-06 22:45:15 -0400

    json

--- src/pages/Vision.svelte
+++ src/pages/Vision.svelte
@@ -5,13 +5,5 @@
 <h2>Vision</h2>
 
 <p class="uk-dropcap">
-  {res.data.description}asdfffffffffffffffffffffffffffffffasdfa asdf asdfa sdfa
-  sdfa sdfasdfadsfasd fasdfasdfa sdfasdfasdf asdfasdfasdf asdfa sdf asdfasdf
-  asdf asdfasdfasdf sadfasdfadsfasd fasdfasdfa sdfasdfasdf asdfasdfasdf asdfa
-  sdf asdfasdf asdf asdfasdfasdf sadfasdfadsfasd fasdfasdfa sdfasdfasdf
-  asdfasdfasdf asdfa sdf asdfasdf asdf asdfasdfasdf sadfasdfadsfasd fasdfasdfa
-  sdfasdfasdf asdfasdfasdf asdfa sdf asdfasdf asdf asdfasdfasdf sadfasdfadsfasd
-  fasdfasdfa sdfasdfasdf asdfasdfasdf asdfa sdf asdfasdf asdf asdfasdfasdf
-  sadfasdfadsfasd fasdfasdfa sdfasdfasdf asdfasdfasdf asdfa sdf asdfasdf asdf
-  asdfasdfasdf sa
+  {res.data.description}
 </p>
